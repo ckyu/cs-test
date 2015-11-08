6db655ba1230cc37213e9a0c5f11e1962982618c
China <chinakimberly.yu@gmail.com>
2015-11-08 08:06:14 +0000

    Lab report data can be saved to database

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,17 +13,18 @@ class ReportsController < ApplicationController
     @patient = Patient.find_by(ssn: ssn)
     @reports = payload[ssn]
 
-    # @reports.each do |raw_report_data|
-    #   report = Report.new
-    #   report.name = 
-    # end
-
-      # render :text => "patient: #{@patient.name}<br>\n 
-                       # report: #{reports}"
-    
-    # end
-    # @report = Report.new(report_params)
-    # render :text => "@var: #{@report.name}"
+    @reports.each do |name, raw_report_data| 
+      report = @patient.reports.build
+      report.name = name
+      report.physician = raw_report_data[:physician] || ""
+      report.date = raw_report_data[:date] || ""
+      report.location = raw_report_data[:location] || ""
+      if report.save
+        logger.debug "Report saved!"
+      else
+        logger.debug "Something went wrong!"
+      end
+    end
   end
 
   private
